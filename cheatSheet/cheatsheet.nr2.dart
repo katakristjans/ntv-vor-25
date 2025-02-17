fdae9da948bb5c9dfd7fbe4537d009776131a23f
katrinkristjans <katrinosk02@gmail.com>
2025-02-17 20:49:07 +0000

    update1

--- cheatSheet/cheatsheet.nr2.dart
+++ cheatSheet/cheatsheet.nr2.dart
@@ -33,7 +33,39 @@ void main(List<String> arguments) {
 
   String username = names[0] + names[1].substring(0,1) + lastName.substring(0,3);
   username = username.toLowerCase();
+  username = removeIcelandicLetters(username);
   print(username);
   String emailAddress = username + "@" + url;
   print(emailAddress);
+}
+
+String removeIcelandicLetters(String input) {
+  Map<String, String> icelandicToLatin = {
+    'ð': 'd',
+    'þ': 'th',
+    'á': 'a',
+    'é': 'e',
+    'í': 'i',
+    'ó': 'o',
+    'ú': 'u',
+    'ý': 'y',
+    'ö': 'o',
+    'Æ': 'Ae',
+    'æ': 'ae',
+    'Ð': 'D',
+    'Þ': 'Th',
+    'Á': 'A',
+    'É': 'E',
+    'Í': 'I',
+    'Ó': 'O',
+    'Ú': 'U',
+    'Ý': 'Y',
+    'Ö': 'O'
+  };
+
+  icelandicToLatin.forEach((key, value) {
+    input = input.replaceAll(key, value);
+  });
+
+  return input;
 }
