ee8f8c76465cfc4caf87262b9dc128118d14a584
katrinkristjans <katrinosk02@gmail.com>
2025-02-17 20:22:02 +0000

    github1

---
+++ cheatSheet/cheatsheet.nr2.dart
@@ -0,0 +1,39 @@
+import 'dart:io';
+
+void main(List<String> arguments) {
+ /* print("Hvað er uppáhalds talan þín?");
+  String? inputNumber = stdin.readLineSync().toString();
+  print("Hún er $inputNumber");
+  int result = 5 * int.parse(inputNumber);
+  print("5 sinnum $inputNumber er $result");
+
+  int number1 = 8;
+  int number2 = 3;
+  double result2 = number1 / number2;
+  print(result2.toStringAsFixed(3));
+
+
+  print("Hæ hvað heitir þú");
+  String firstName = stdin.readLineSync().toString();
+  print("Halló $firstName hvers dóttir ert þú?");
+  String lastName = stdin.readLineSync().toString();
+  firstName = firstName.replaceAll(firstName, "Jón");
+  print("Halló $firstName $lastName");
+*/
+
+  String firstName = "Katrín Ósk";
+  String lastName = "Kristjánsdóttir";
+  String fullName = firstName + " " + lastName;
+  String url = "ntv.is";
+
+  List<String> names = firstName.split(" ");
+  print(names);
+  print(names[0]);
+  print(names[1]);
+
+  String username = names[0] + names[1].substring(0,1) + lastName.substring(0,3);
+  username = username.toLowerCase();
+  print(username);
+  String emailAddress = username + "@" + url;
+  print(emailAddress);
+}
